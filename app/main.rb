2c874d80192f835a3c3e04c89133d3131926ae07
Kehvarl <Kehvarl@Kehvarl.com>
2022-11-03 17:00:05 -0700

    Create Game Map and Tiles. Allow tile removal

--- app/main.rb
+++ app/main.rb
@@ -1,6 +1,6 @@
-def tick args
-  args.outputs.labels  << [640, 500, 'Hello World!', 5, 1]
-  args.outputs.labels  << [640, 460, 'Go to docs/docs.html and read it!', 5, 1]
-  args.outputs.labels  << [640, 420, 'Join the Discord! https://discord.dragonruby.org', 5, 1]
-  args.outputs.sprites << [576, 280, 128, 101, 'dragonruby.png']
-end
+# Load classes here
+
+require 'app/game_map.rb'
+
+# Do this last so you know all your classes are loaded before the game starts
+require 'app/tick.rb'
---
+++ app/tick.rb
@@ -0,0 +1,15 @@
+def init args
+  args.state.game_map = Game_Map.new()
+end
+
+def tick args
+  if args.state.tick_count == 0
+    init args
+  end
+  args.outputs.primitives << args.state.game_map.render
+
+  if rand(100) <= 20
+    args.state.game_map.tiles[rand(71)][rand(127)] = nil
+  end
+end
+
