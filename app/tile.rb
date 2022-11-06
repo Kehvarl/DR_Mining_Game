6bf819b7e4320604254cfef238ea544abd18fa8e
Kehvarl <Kehvarl@Kehvarl.com>
2022-11-06 12:47:48 -0700

    Working on fog.

--- app/tile.rb
+++ app/tile.rb
@@ -5,7 +5,7 @@ class Tile < Sprite
     self.y = y*10
     self.w = 10
     self.h = 10
-    self.path = 'sprites/tile/wall-1111.png'
+    self.path = 'sprites/tile/fog-001.png'
     #self.r = 128
     #self.g = 128
     #self.b = 129
