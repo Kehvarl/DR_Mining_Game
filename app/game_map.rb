3ffbd5968a9be33ac49a1406c0e8ee04692460a5
Kehvarl <Kehvarl@Kehvarl.com>
2022-11-05 17:07:51 -0700

    Tiles are Sprites not Solids

--- app/game_map.rb
+++ app/game_map.rb
@@ -6,6 +6,10 @@ class Solid
   end
 end
 
+class Sprite
+  attr_sprite
+end
+
 class Background < Solid
   def initialize
     super
@@ -20,17 +24,18 @@ class Background < Solid
   end
 end
 
-class Tile < Solid
+class Tile < Sprite
   def initialize (x, y)
     super
     self.x = x*10
     self.y = y*10
     self.w = 10
     self.h = 10
-    self.r = 128
-    self.g = 128
-    self.b = 129
-    self.a = 255
+    self.path = 'sprites/tile/wall-1111.png'
+    #self.r = 128
+    #self.g = 128
+    #self.b = 129
+    #self.a = 255
   end
 end
 
