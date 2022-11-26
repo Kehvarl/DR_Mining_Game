eeb3f4b5e3693e5ddfe5c39b525cf3b36f2de26f
Kehvarl <Kehvarl@Kehvarl.com>
2022-11-25 17:34:56 -0700

    thinking

--- app/tick.rb
+++ app/tick.rb
@@ -54,19 +54,6 @@ def handle_keys (args)
   end
 end
 
-def tile_remove(args)
-  if rand(100) < 33
-    x = rand(args.state.game_map.h)
-    y = rand(args.state.game_map.w)
-    v = args.state.game_map.tiles[x][y].visible
-    if v
-      args.state.game_map.tiles[x][y].dig
-    else
-      args.state.game_map.tiles[x][y].set_visible
-    end
-  end
-end
-
 def tick(args)
   if args.state.tick_count == 0
     init args
